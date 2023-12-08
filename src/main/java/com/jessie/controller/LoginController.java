a58303403157be30a70234468da7dd62924d580c
yueyijessie <welhomejessie@gmail.com>
2023-12-08 14:17:02 +0800

    员工登录jwt生成和下发

--- src/main/java/com/jessie/controller/LoginController.java
+++ src/main/java/com/jessie/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.jessie.controller;
 import com.jessie.pojo.Emp;
 import com.jessie.pojo.Result;
 import com.jessie.service.EmpService;
+import com.jessie.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -20,7 +24,20 @@ public class LoginController {
     public Result login(@RequestBody Emp emp){
         log.info("员工登录,{}", emp);
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+
+        if (e != null) {
+            // 登录成功，生成令牌并下发
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims); // jwt包含当前登录的与昂工信息
+            return Result.success(jwt);
+        } else {
+            // 登录失败，返回错误信息
+            return Result.error("用户名或密码错误");
+        }
     }
 
 
